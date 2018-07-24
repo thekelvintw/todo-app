a7a11c0bdc625e83bfff00dda7542dadec11a957
thekelvintw <kelvin.aiesec@gmail.com>
2018-07-24 15:02:31 -0700

    saving to do to local storage

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 	console.log('+++line2 ,file main js', $)
 	
+	// Saving todo to local storage
+	var i =0;
 	$('#writeTodoBox').on('keydown',function(event){
-		var $TodoBox = $('#writeTodoBox');
+		var $TodoBox = $('#writeTodoBox')
 		if(event.keyCode===13){
-			console.log($TodoBox.val())
-		}
+			var todoStorage = localStorage.setItem('todo' + i, $TodoBox.val())
+		console.log(i)	
+		i=i+1
+		}		
 	})
 
 })
