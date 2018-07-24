5a3444b2190127dd0f9b77e8dc605bf600e9405b
thekelvintw <kelvin.aiesec@gmail.com>
2018-07-24 15:15:05 -0700

    get todo i on screen

--- main.js
+++ main.js
@@ -6,10 +6,16 @@ $(document).ready(function(){
 	$('#writeTodoBox').on('keydown',function(event){
 		var $TodoBox = $('#writeTodoBox')
 		if(event.keyCode===13){
-			var todoStorage = localStorage.setItem('todo' + i, $TodoBox.val())
+			localStorage.setItem('todo' + i, $TodoBox.val())
 		console.log(i)	
 		i=i+1
-		}		
+		}
+		var todoItem
+		$('.todoList').text(localStorage.getItem('todo' + i));
+
 	})
+
+
+
 
 })
